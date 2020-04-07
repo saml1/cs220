7a1fea0a783920ee76766ea15a155b0a8a1f4b55
Sam Lipschitz <slipsch3@jhu.edu>
2020-04-07 11:14:24 -0400

    finished Makefile, barely started hw5.cpp

--- homework/hw5/hw5.cpp
+++ homework/hw5/hw5.cpp
@@ -1,2 +1,12 @@
 //Sam Lipschitz
 //slipsch3
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char* argv[]){
+  using std::cin;
+  using std::cout;
+  using std::endl;
+  cout << "hi" << endl;
+}
